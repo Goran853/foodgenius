30312ce0f677b34b07b5ffb93548f450b972fdbf
Goran Levanic <glevanic@gmail.com>
2024-04-21 16:48:12 +0200

    feat: add brand colors to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+    },
+    fontWeight: {
+      default: '300',
+      medium: '500',
+      strong: '700',
+    },
+    extend: {
+      colors: {
+        'brand-primary': '#991530',
+        'brand-secondary': '#5D5D5D',
+        'gray-light': '#f5f6f7',
+        'gray-medium': '#e9e9e9',
+      },
+    },
   },
   plugins: [],
 };
