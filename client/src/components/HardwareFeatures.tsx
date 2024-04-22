b89035ddc5c78378d5a610fe1de739ddccd51160
Goran Levanic <glevanic@gmail.com>
2024-04-22 14:24:21 +0200

    feat: implement responsiveness for >md HardwareFeatures component

--- client/src/components/HardwareFeatures.tsx
+++ client/src/components/HardwareFeatures.tsx
@@ -7,8 +7,8 @@ function HardwareFeatures({ title, description }: HardwareFeatureProps) {
   return (
     <div className="flex flex-col gap-40">
       <div className="flex flex-col gap-6 text-center">
-        <h3 className="md:text-xl text-2xl">{title}</h3>
-        <p className="md:text-sm">{description}</p>
+        <h3 className="lg:text-xl md:text-sm text-2xl">{title}</h3>
+        <p className="lg:text-sm md:text-xs">{description}</p>
       </div>
     </div>
   );
