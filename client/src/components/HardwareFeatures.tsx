82bdbe28bd93a25189032eb5d87c5409ec333c2e
Goran Levanic <glevanic@gmail.com>
2024-04-22 18:17:24 +0200

    feat: finish HardwareFeatures responsive design

--- client/src/components/HardwareFeatures.tsx
+++ client/src/components/HardwareFeatures.tsx
@@ -7,8 +7,12 @@ function HardwareFeatures({ title, description }: HardwareFeatureProps) {
   return (
     <div className="flex flex-col gap-40">
       <div className="flex flex-col gap-6 text-center">
-        <h3 className="lg:text-xl md:text-base text-2xl">{title}</h3>
-        <p className="lg:text-sm md:text-xs">{description}</p>
+        <h3 className="text-2xl md:text-base lg:text-2xl xl:text-3xl 2xl:text-4xl">
+          {title}
+        </h3>
+        <p className="md:text-xs lg:text-sm xl:text-lg 2xl:text-xl">
+          {description}
+        </p>
       </div>
     </div>
   );
