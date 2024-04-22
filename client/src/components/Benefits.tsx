1c9cab30793351fc3e0e6900c086a5fdf6ea7672
Goran Levanic <glevanic@gmail.com>
2024-04-22 14:24:11 +0200

    feat: implement responsiveness for >md Benefits component

--- client/src/components/Benefits.tsx
+++ client/src/components/Benefits.tsx
@@ -20,23 +20,28 @@ function Benefits() {
   ];
 
   return (
-    <div className="pt-32 max-container">
-      <div className="grid md:grid-cols-3 md:gap-8 gap-48 ">
+    <div className="pt-32 md:pt-0 max-container">
+      {/* Benefits */}
+      <div className="grid md:grid-cols-3 lg:gap-8 md:gap-4 gap-48 ">
         {benefits.map((benefit) => (
           <div key={benefit.title} className="flex flex-col">
             <h3 className="lg:text-4xl md:text-2xl text-3xl">
               {benefit.title}
             </h3>
-            <p className="lg:text-lg md:text-sm mt-10">{benefit.description}</p>
+            <p className="lg:text-lg md:text-xs mt-10">{benefit.description}</p>
           </div>
         ))}
       </div>
-      <Link
-        to="/"
-        className="border-2 border-black rounded-full py-2 mt-12 block "
-      >
-        alle vorteile ansehen
-      </Link>
+
+      {/* Button */}
+      <div className="mt-10">
+        <Link
+          to="/"
+          className="border-2 border-black rounded-full py-2 mt-12 block md:inline md:text-xs md:px-6 md:py-1"
+        >
+          alle vorteile ansehen
+        </Link>
+      </div>
     </div>
   );
 }
