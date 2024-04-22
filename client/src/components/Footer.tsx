d4b3b56fe52cc9b62b619a8ed0bd94f7db5e3ef4
Goran Levanic <glevanic@gmail.com>
2024-04-22 18:15:14 +0200

    feat: finish responsive design for footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -59,19 +59,31 @@ const footerLinks: FooterLink[] = [
 
 function Contact({ department, phone, availability, email }: ContactProps) {
   return (
-    <div className="flex flex-col gap-1 md:pb-8 items-center md:items-start">
-      <p className="lg:text-2xl md:text-sm font-strong ">{department}</p>
+    <div className="flex flex-col gap-1 items-center md:pb-8 md:items-start 2xl:gap-2">
+      <p className="font-strong md:text-sm lg:text-xl xl:text-2xl">
+        {department}
+      </p>
 
       <div className="flex gap-4">
-        <img src={phoneIcon} alt="Handy" className="w-4 md:w-2" />
-        <p className="lg:text-xl md:text-sm">{phone}</p>
+        <img
+          src={phoneIcon}
+          alt="Handy"
+          className="w-4 md:w-2 lg:w-3 2xl:w-4"
+        />
+        <p className="md:text-sm lg:text-xl xl:text-2xl">{phone}</p>
       </div>
 
-      <p className="lg:text-lg md:text-xs text-sm">{availability}</p>
+      <p className="text-sm md:text-xs lg:text-lg 2xl:text-xl">
+        {availability}
+      </p>
 
       {email && (
-        <div className="flex gap-4 underline lg:text-xl md:text-sm">
-          <img src={emailWhite} alt="Handy" className="w-6 md:w-3" />
+        <div className="flex gap-4 underline md:text-sm lg:text-xl 2xl:text-2xl">
+          <img
+            src={emailWhite}
+            alt="Handy"
+            className="w-6 md:w-3 lg:w-4 xl:w-5 2xl:w-6"
+          />
           <p>{email}</p>
         </div>
       )}
@@ -81,15 +93,18 @@ function Contact({ department, phone, availability, email }: ContactProps) {
 
 function Footer() {
   return (
-    <footer className="bg-brand-primary text-white text-lg w-screen py-6">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-8 md:p-0 w-full max-w-full md:max-container mx-auto">
+    <footer className="bg-brand-primary py-6 text-white text-lg w-screen">
+      <div className="grid grid-cols-1 gap-6 max-w-full mx-auto py-8 w-full md:grid-cols-3 md:max-container md:p-0">
+        {/* Footer Logo */}
         <div className="flex justify-center w-full">
           <img
             src={logoFooter}
             alt="Foodgenius logo"
-            className="object-contain self-start w-16"
+            className="object-contain self-start w-16 xl:w-24"
           />
         </div>
+
+        {/* Contacts */}
         <div className="flex flex-col gap-4 md:gap-0">
           {contacts.map((contact) => (
             <Contact key={contact.department} {...contact} />
@@ -97,7 +112,7 @@ function Footer() {
         </div>
 
         {/* Links */}
-        <ul className="flex flex-wrap md:flex-col justify-center gap-6 underline md:items-center md:self-start md:text-right md:text-sm">
+        <ul className="flex flex-wrap justify-center gap-6 underline md:flex-col md:items-center md:self-start md:text-right md:text-sm lg:text-xl xl:text-2xl">
           {footerLinks.map((link) => (
             <li key={link.name}>
               <Link to={link.url} key={link.name}>
@@ -105,7 +120,7 @@ function Footer() {
                   <img
                     src={link.imgSrc}
                     alt={link.imgAlt}
-                    className="w-4 md:w-2"
+                    className="w-4 md:w-2 xl:w-4"
                   />
                 ) : (
                   link.name
